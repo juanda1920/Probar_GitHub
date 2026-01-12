180bdda083fe82fb165d1f74b4745f364e875d22
juanda1920 <acostajuandaniel19@gmail.com>
2026-01-12 16:16:41 -0500

    Primer commit

---
+++ src/negocio/Persona.java
@@ -0,0 +1,43 @@
+package negocio;
+
+public class Persona {
+    private int edad;
+    private String nombre;
+    private String correo;
+
+    public Persona(int edad, String nombre, String correo) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Correo: " + correo);
+    }
+}
